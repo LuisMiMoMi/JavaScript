36652b89b879390190e4419f7b257f720fa88a08
= <=>
2020-10-27 19:52:43 +0100

    actualizacion 27 oct

---
+++ js/prueba.js
@@ -0,0 +1,42 @@
+(() => {
+    "use strict";
+  
+    document.addEventListener("DOMContentLoaded", function () {
+        generarApartados();
+        
+    });
+})();
+
+function generarApartados() {
+    var cuerpo = document.getElementsByTagName("body")[0];
+    var contenedorInicial = document.createElement("div");
+    var contenedor1 = document.createElement("div");
+    var contenedor2 = document.createElement("div");
+    var contenedor3 = document.createElement("div");
+    contenedorInicial.id="divInicial";
+    contenedor1.id="contenedor1";
+    contenedor2.id="contenedor2";
+    contenedor3.id="contenedor3";
+    contenedorInicial.appendChild(contenedor1);
+    contenedorInicial.appendChild(contenedor2);
+    contenedorInicial.appendChild(contenedor3);
+    cuerpo.appendChild(contenedorInicial);
+    for (let index = 0; index < 3; index++) {
+        var ulCuadrados = document.createElement("ul");
+        ulCuadrados.className="cuadrados";
+        if (index == 0) {
+            contenedor1.appendChild(ulCuadrados);
+        } else if (index == 1) {
+            contenedor2.appendChild(ulCuadrados);
+        } else {
+            contenedor3.appendChild(ulCuadrados);
+        }
+        for (let i = 0; i < 10; i++) {
+            var li = document.createElement("li");
+            ulCuadrados.appendChild(li);
+        }
+        
+        
+    }
+    
+}
--- js/script.js
+++ js/script.js
@@ -2,41 +2,25 @@
     "use strict";
   
     document.addEventListener("DOMContentLoaded", function () {
-        generarApartados();
-        
+        $('#login-button').click(function(){
+            $('#login-button').fadeOut("slow",function(){
+              $("#container").fadeIn();
+              TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
+              TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+            });
+          });
+          
+          $(".close-btn").click(function(){
+            TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
+            TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+            $("#container, #forgotten-container").fadeOut(800, function(){
+              $("#login-button").fadeIn(800);
+            });
+          });
+
+          $("a[href^=\"#\"]").click(function () {
+            TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
+            TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+          });
     });
 })();
-
-function generarApartados() {
-    var cuerpo = document.getElementsByTagName("body")[0];
-    var contenedorInicial = document.createElement("div");
-    var contenedor1 = document.createElement("div");
-    var contenedor2 = document.createElement("div");
-    var contenedor3 = document.createElement("div");
-    contenedorInicial.id="divInicial";
-    contenedor1.id="contenedor1";
-    contenedor2.id="contenedor2";
-    contenedor3.id="contenedor3";
-    contenedorInicial.appendChild(contenedor1);
-    contenedorInicial.appendChild(contenedor2);
-    contenedorInicial.appendChild(contenedor3);
-    cuerpo.appendChild(contenedorInicial);
-    for (let index = 0; index < 3; index++) {
-        var ulCuadrados = document.createElement("ul");
-        ulCuadrados.className="cuadrados";
-        if (index == 0) {
-            contenedor1.appendChild(ulCuadrados);
-        } else if (index == 1) {
-            contenedor2.appendChild(ulCuadrados);
-        } else {
-            contenedor3.appendChild(ulCuadrados);
-        }
-        for (let i = 0; i < 10; i++) {
-            var li = document.createElement("li");
-            ulCuadrados.appendChild(li);
-        }
-        
-        
-    }
-    
-}
