81ad08cc00cc73ab1ff2338d184c9b87ecb6f654
= <=>
2020-10-27 20:33:21 +0100

    sdfg

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
   
     document.addEventListener("DOMContentLoaded", function () {
         login();
+        generarApartados();
       });
 })();
 
@@ -17,12 +18,29 @@ function login() {
   $(".close-btn").click(function(){
     TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
     TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
-    $("#container, #forgotten-container").fadeOut(800, function(){
+    $("#container").fadeOut(800, function(){
       $("#login-button").fadeIn(800);
     });
   });
   $("a[href^=\"#\"]").click(function () {
     TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
     TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+    $("#container").fadeOut(800, function(){
+      $("#login-button").fadeIn(800);
+    });
   });
+}
+function generarApartados() {
+ var contenedorInicial = document.getElementById("contenido");
+  var contenedor1 = document.createElement("div");
+  var contenedor2 = document.createElement("div");
+  var contenedor3 = document.createElement("div");
+  contenedor1.id="contenedor1";
+  contenedor2.id="contenedor2";
+  contenedor3.id="contenedor3";
+  contenedorInicial.appendChild(contenedor1);
+  contenedorInicial.appendChild(contenedor2);
+  contenedorInicial.appendChild(contenedor3);
+
+  
 }
